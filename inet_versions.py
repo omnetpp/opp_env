f93b5e9e62ac8a9d40b3f9d578d4b6971476dbf5
Rudolf Hornig <rudi@omnetpp.org>
2023-01-12 15:54:27 +0100

    updated INET and OMNETPP database. Added commonly used INET versions with proper OMNET dependencies.

--- inet_versions.py
+++ inet_versions.py
@@ -3,52 +3,96 @@ description = "INET Framework is an open-source OMNeT++ model suite for wired, w
 def get_all_inet_4_x_versions():
     return [
         {
-            "name": "inet", "version": "4.4", "description": description,
+            "name": "inet", "version": "4.4.1", "description": description,
             "folder_name": "inet",
-            "required_projects": {"omnetpp":  ["6.0", "5.7"]},
-            "external_nix_packages": ["z3"],
-            "download_command": "git clone -b v4.4.0 git@github.com:inet-framework/inet.git inet-4.4",
+            "required_projects": {"omnetpp":  [ "6.0.1" ]},
+            "external_nix_packages": [ "python3", "z3"],
+            "download_command": "git clone -b v4.4.1 git@github.com:inet-framework/inet.git inet-4.4.1",
             "setenv_command": "source setenv",
-            "build_command": "make makefiles && make -j16 MODE=release",
+            "build_command": "make makefiles && make -j$NIX_BUILD_CORES MODE=release",
             "clean_command": "make clean"
         },
         {
-            "name": "inet", "version": "4.3.5", "description": description,
+            "name": "inet", "version": "4.3.9", "description": description,
             "folder_name": "inet",
-            "required_projects": {"omnetpp":  ["6.0", "5.7"]},
-            "external_nix_packages": ["z3"],
-            "download_command": "git clone -b v4.3.5 git@github.com:inet-framework/inet.git inet-4.3.5",
+            "required_projects": {"omnetpp":  [ "6.0.1" ]},
+            "external_nix_packages": [ "python3", "z3"],
+            "download_command": "git clone -b v4.3.9 git@github.com:inet-framework/inet.git inet-4.3.9",
             "setenv_command": "source setenv",
             "configure_command": "make makefiles",
-            "build_command": "make -j16 MODE=release",
+            "build_command": "make -j$NIX_BUILD_CORES MODE=release",
             "clean_command": "make clean"
         },
         {
-            "name": "inet", "version": "4.2.2", "description": description,
+            "name": "inet", "version": "4.2.10", "description": description,
             "folder_name": "inet",
-            "required_projects": {"omnetpp":  ["5.6.2"]},
-            "external_nix_packages": ["z3"],
-            "download_command": "git clone -b v4.2.2 git@github.com:inet-framework/inet.git inet-4.2.2",
+            "required_projects": {"omnetpp":  [ "6.0.1", "5.7" ]},
+            "external_nix_packages": [ "python3", "z3"],
+            "download_command": "git clone -b v4.2.10 git@github.com:inet-framework/inet.git inet-4.2.10",
             "setenv_command": "source setenv -f",
             "configure_command": "make makefiles",
-            "build_command": "make -j16 MODE=release",
+            "build_command": "make -j$NIX_BUILD_CORES MODE=release",
             "clean_command": "make clean"
         },
         {
-            "name": "inet", "version": "4.2", "description": description,
+            "name": "inet", "version": "4.2.5", "description": description,
             "folder_name": "inet",
-            "required_projects": {"omnetpp":  ["5.7"]},
-            "external_nix_packages": ["z3"],
-            "download_command": "git clone -b v4.2.0 git@github.com:inet-framework/inet.git inet-4.2",
+            "required_projects": {"omnetpp":  [ "5.6.2", "5.5.1", "5.4.1" ]},
+            "external_nix_packages": [ "python3", "z3"],
+            "download_command": "git clone -b v4.2.5 git@github.com:inet-framework/inet.git inet-4.2.5",
             "setenv_command": "source setenv -f",
             "configure_command": "make makefiles",
-            "build_command": "make -j16 MODE=release",
+            "build_command": "make -j$NIX_BUILD_CORES MODE=release",
             "clean_command": "make clean"
         },
     ]
 
 def get_all_inet_3_x_versions():
     return [
+        {
+            "name": "inet", "version": "3.8.3", "description": description,
+            "folder_name": "inet",
+            "required_projects": {"omnetpp":  [ "6.0.1", "5.7" ]},
+            "external_nix_packages": [ "python3" ],
+            "download_command": "git clone -b v3.8.3 git@github.com:inet-framework/inet.git inet-3.8.3",
+            "setenv_command": "source setenv -f",
+            "configure_command": "make makefiles",
+            "build_command": "make -j$NIX_BUILD_CORES MODE=release",
+            "clean_command": "make clean"
+        },
+        {
+            "name": "inet", "version": "3.7.1", "description": description,
+            "folder_name": "inet",
+            "required_projects": {"omnetpp":  [ "5.7", "5.6.2", "5.5.1", "5.4.1", "5.3" ]},
+            "external_nix_packages": [ "python3" ],
+            "download_command": "git clone -b v3.7.1 git@github.com:inet-framework/inet.git inet-3.7.1",
+            "setenv_command": "source setenv -f",
+            "configure_command": "make makefiles",
+            "build_command": "make -j$NIX_BUILD_CORES MODE=release",
+            "clean_command": "make clean"
+        },
+        {
+            "name": "inet", "version": "3.3.0", "description": description,
+            "folder_name": "inet",
+            "required_projects": {"omnetpp":  [ "4.6" ]},
+            "external_nix_packages": [ "python3" ],
+            "download_command": "git clone -b v3.3.0 git@github.com:inet-framework/inet.git inet-3.3.0",
+            "setenv_command": "true",
+            "configure_command": "make makefiles",
+            "build_command": "make -j$NIX_BUILD_CORES MODE=release",
+            "clean_command": "make clean"
+        },
+        {
+            "name": "inet", "version": "2.6.0", "description": description,
+            "folder_name": "inet",
+            "required_projects": {"omnetpp":  [ "4.4.1" ]},
+            "external_nix_packages": [ ],
+            "download_command": "git clone -b v2.6.0 git@github.com:inet-framework/inet.git inet-2.6.0",
+            "setenv_command": "",
+            "configure_command": "make makefiles",
+            "build_command": "make -j$NIX_BUILD_CORES MODE=release",
+            "clean_command": "make clean"
+        },
     ]
 
 def get_all_inet_versions():
@@ -57,10 +101,10 @@ def get_all_inet_versions():
             "name": "inet", "version": "git", "description": description,
             "folder_name": "inet",
             "required_projects": {"omnetpp":  ["git"]},
-            "external_nix_packages": ["z3"],
+            "external_nix_packages": [ "python3", "z3"],
             "download_command": "git git@github.com:inet-framework/inet.git inet",
             "setenv_command": "source setenv",
-            "build_command": "make makefiles && make -j16 MODE=release",
+            "build_command": "make makefiles && make -j$NIX_BUILD_CORES MODE=release",
             "clean_command": "make clean"
         },
         *get_all_inet_4_x_versions(),
--- omnetpp_versions.py
+++ omnetpp_versions.py
@@ -9,15 +9,6 @@ def get_all_omnetpp_6_x_versions():
             "build_command": "make -j$NIX_BUILD_CORES MODE=release",
             "clean_command": "make clean"
         },
-        {
-            "name": "omnetpp", "version": "6.0",
-            "external_nix_packages": ["ccache", "bison", "flex", "perl", "which", "xdg-utils", "qt5.qtbase", "qt5.qtsvg", "python3", "python3Packages.numpy", "python3Packages.scipy", "python3Packages.pandas", "python3Packages.matplotlib", "python3Packages.posix_ipc"],
-            "download_command": "wget -q -nv --show-progress https://github.com/omnetpp/omnetpp/releases/download/omnetpp-6.0/omnetpp-6.0-linux-x86_64.tgz && tar xzf omnetpp-6.0-linux-x86_64.tgz && rm omnetpp-6.0-linux-x86_64.tgz",
-            "setenv_command": "source setenv",
-            "configure_command": "./configure WITH_OSG=no",
-            "build_command": "make -j$NIX_BUILD_CORES MODE=release",
-            "clean_command": "make clean"
-        },
     ]
 
 def get_all_omnetpp_5_x_versions():
@@ -41,18 +32,27 @@ def get_all_omnetpp_5_x_versions():
             "clean_command": "make clean"
         },
         {
-            "name": "omnetpp", "version": "5.6.1",
+            "name": "omnetpp", "version": "5.5.1",
             "external_nix_packages": ["ccache", "bison", "flex", "perl", "which", "xdg-utils", "python3", "qt5.qtbase", "qt5.qtsvg" ],
-            "download_command": "wget -q -nv --show-progress https://github.com/omnetpp/omnetpp/releases/download/omnetpp-5.6.1/omnetpp-5.6.1-src-linux.tgz && tar xzf omnetpp-5.6.1-src-linux.tgz && rm omnetpp-5.6.1-src-linux.tgz",
+            "download_command": "wget -q -nv --show-progress https://github.com/omnetpp/omnetpp/releases/download/omnetpp-5.5.1/omnetpp-5.5.1-src-linux.tgz && tar xzf omnetpp-5.5.1-src-linux.tgz && rm omnetpp-5.5.1-src-linux.tgz",
             "setenv_command": "source setenv -f",
             "configure_command": "unset AR && ./configure WITH_OSG=no WITH_OSGEARTH=no",
             "build_command": "make -j$NIX_BUILD_CORES MODE=release",
             "clean_command": "make clean"
         },
         {
-            "name": "omnetpp", "version": "5.6",
+            "name": "omnetpp", "version": "5.4.1",
             "external_nix_packages": ["ccache", "bison", "flex", "perl", "which", "xdg-utils", "python3", "qt5.qtbase", "qt5.qtsvg" ],
-            "download_command": "wget -q -nv --show-progress https://github.com/omnetpp/omnetpp/releases/download/omnetpp-5.6/omnetpp-5.6-src-linux.tgz && tar xzf omnetpp-5.6-src-linux.tgz && rm omnetpp-5.6-src-linux.tgz",
+            "download_command": "wget -q -nv --show-progress https://github.com/omnetpp/omnetpp/releases/download/omnetpp-5.4.1/omnetpp-5.4.1-src-linux.tgz && tar xzf omnetpp-5.4.1-src-linux.tgz && rm omnetpp-5.4.1-src-linux.tgz",
+            "setenv_command": "source setenv -f",
+            "configure_command": "unset AR && ./configure WITH_OSG=no WITH_OSGEARTH=no",
+            "build_command": "make -j$NIX_BUILD_CORES MODE=release",
+            "clean_command": "make clean"
+        },
+        {
+            "name": "omnetpp", "version": "5.3",
+            "external_nix_packages": ["ccache", "bison", "flex", "perl", "which", "xdg-utils", "python3", "qt5.qtbase", "qt5.qtsvg" ],
+            "download_command": "wget -q -nv --show-progress https://github.com/omnetpp/omnetpp/releases/download/omnetpp-5.3/omnetpp-5.3-src-linux.tgz && tar xzf omnetpp-5.3-src-linux.tgz && rm omnetpp-5.3-src-linux.tgz",
             "setenv_command": "source setenv -f",
             "configure_command": "unset AR && ./configure WITH_OSG=no WITH_OSGEARTH=no",
             "build_command": "make -j$NIX_BUILD_CORES MODE=release",
@@ -66,6 +66,15 @@ def get_all_omnetpp_4_x_versions():
             "name": "omnetpp", "version": "4.6",
             "external_nix_packages": [ "bison", "flex", "perl", "tk", "tcl", "expat", "xdg-utils" ],
             "download_command": "wget -q -nv --show-progress https://github.com/omnetpp/omnetpp/releases/download/omnetpp-4.6/omnetpp-4.6-src.tgz && tar xzf omnetpp-4.6-src.tgz && rm omnetpp-4.6-src.tgz",
+            "setenv_command": "export PATH=$(pwd)/bin:$PATH && export LD_LIBRARY_PATH=$(pwd)/lib:$LD_LIBRARY_PATH && export TCL_LIBRARY=$(echo 'puts [info library]; exit' | wish) && export HOSTNAME && export HOST",
+            "configure_command": "unset AR && ./configure",
+            "build_command": "make -j$NIX_BUILD_CORES MODE=release",
+            "clean_command": "make clean"
+        },
+        {
+            "name": "omnetpp", "version": "4.4.1",
+            "external_nix_packages": [ "bison", "flex", "perl", "tk", "tcl", "expat", "xdg-utils" ],
+            "download_command": "wget -q -nv --show-progress https://github.com/omnetpp/omnetpp/releases/download/omnetpp-4.4.1/omnetpp-4.4.1-src.tgz && tar xzf omnetpp-4.4.1-src.tgz && rm omnetpp-4.4.1-src.tgz",
             "setenv_command": "export PATH=$(pwd)/bin:$PATH && export LD_LIBRARY_PATH=$(pwd)/lib:$LD_LIBRARY_PATH && export TCL_LIBRARY=$(echo 'puts [info library]; exit' | wish) && export HOSTNAME && export HOST",
             "configure_command": "unset AR && ./configure",
             "build_command": "make -j$NIX_BUILD_CORES MODE=release",

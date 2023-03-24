4a1e7189bc49c1a2df7c200054c2364fd189fe22
Andras Varga <andras@omnetpp.org>
2023-03-24 11:44:47 +0100

    omnetpp_versions.py: various fixes
    - fix: earlier 4.x releases are at a different download URL
    - fix wrong dowload URL for omnetpp 5.0. 5.1, 5.2; merge with 5.7
    - build omnetpp 4.x with gcc7Stdenv, too, yields way less warnings during compilation than more recent gcc versions.
    - added libxml2; tcl/tk to 4.x
    - remove AR hack
    - ...

--- opp_env/omnetpp_versions.py
+++ opp_env/omnetpp_versions.py
@@ -1,3 +1,5 @@
+import re
+
 def get_all_omnetpp_6_x_versions():
     return [
         *[
@@ -18,24 +20,15 @@ def get_all_omnetpp_5_x_versions():
         *[
             {
                 "name": "omnetpp", "version": version,
-                "external_nix_packages": ["ccache", "bison", "flex", "perl", "which", "xdg-utils", "python3", "qt5.qtbase", "qt5.qtsvg" ],
-                "download_url": f"https://github.com/omnetpp/omnetpp/releases/download/omnetpp-{version}/omnetpp-{version}-linux-x86_64.tgz",
-                "setenv_command": "source setenv",
-                "configure_command": "unset AR && ./configure WITH_OSG=no WITH_OSGEARTH=no",
-                "build_command": "make -j$NIX_BUILD_CORES MODE=release",
-                "clean_command": "make clean"
-            } for version in ["5.7"]
-        ],
-        *[
-            {
-                "name": "omnetpp", "version": version,
-                "external_nix_packages": ["ccache", "bison", "flex", "perl", "which", "xdg-utils", "python3", "qt5.qtbase", "qt5.qtsvg" ],
-                "download_url": f"https://github.com/omnetpp/omnetpp/releases/download/omnetpp-{version}/omnetpp-{version}-src-linux.tgz",
+                "external_nix_packages": ["ccache", "bison", "flex", "perl", "which", "xdg-utils", "python3", "tk", "tcl", "qt5.qtbase", "qt5.qtsvg" ],
+                "download_url": f"https://github.com/omnetpp/omnetpp/releases/download/omnetpp-{version}/omnetpp-{version}-linux-x86_64.tgz" if version == "5.7" else
+                                f"https://github.com/omnetpp/omnetpp/releases/download/omnetpp-{version}/omnetpp-{version}-src.tgz" if version == "5.0" else
+                                f"https://github.com/omnetpp/omnetpp/releases/download/omnetpp-{version}/omnetpp-{version}-src-linux.tgz",
                 "setenv_command": "source setenv -f",
-                "configure_command": "unset AR && ./configure WITH_OSG=no WITH_OSGEARTH=no",
+                "configure_command": "./configure WITH_OSG=no WITH_OSGEARTH=no QT_VERSION=5",
                 "build_command": "make -j$NIX_BUILD_CORES MODE=release",
                 "clean_command": "make clean"
-            } for version in ["5.6.2", "5.6.1", "5.6", "5.5.1", "5.4.1", "5.3", "5.2.1", "5.2", "5.1.1", "5.1", "5.0"]
+            } for version in ["5.7", "5.6.2", "5.5.1", "5.4.1", "5.3", "5.2.1", "5.1.1", "5.0"]
         ]
     ]
 
@@ -44,13 +37,15 @@ def get_all_omnetpp_4_x_versions():
         *[
             {
                 "name": "omnetpp", "version": version,
+                "stdenv": "gcc7Stdenv",
                 "external_nix_packages": [ "bison", "flex", "perl", "tk", "tcl", "libxml2", "expat", "xdg-utils" ],
-                "download_url": f"https://github.com/omnetpp/omnetpp/releases/download/omnetpp-{version}/omnetpp-{version}-src.tgz",
-                "setenv_command": "export PATH=$(pwd)/bin:$PATH && export LD_LIBRARY_PATH=$(pwd)/lib:$LD_LIBRARY_PATH && export TCL_LIBRARY=$(echo 'puts [info library]; exit' | wish) && export HOSTNAME && export HOST",
-                "configure_command": "unset AR && ./configure",
+                "download_url": f"https://github.com/omnetpp/omnetpp/releases/download/omnetpp-4.0/omnetpp-4.0p1-src.tgz" if version == '4.0p1' else
+                                f"https://github.com/omnetpp/omnetpp/releases/download/omnetpp-{version}/omnetpp-{version}-src.tgz",
+                "setenv_command": ". setenv",
+                "configure_command": "./configure",
                 "build_command": "make -j$NIX_BUILD_CORES MODE=release",
                 "clean_command": "make clean"
-            } for version in ["4.6", "4.5", "4.4.1", "4.4", "4.3.1", "4.3", "4.2.2", "4.2.1", "4.2", "4.1", "4.0"]
+            } for version in ["4.6", "4.5", "4.4.1", "4.3.1", "4.2.2", "4.1", "4.0p1"]
         ]
     ]
 
@@ -59,10 +54,9 @@ def get_all_omnetpp_3_x_versions():
         {
             "name": "omnetpp", "version": "3.3p1",
             "stdenv": "gcc7Stdenv",
-            "external_nix_packages": [ "bison", "flex", "perl", "tk", "tcl", "expat" ],
+            "external_nix_packages": [ "bison", "flex", "perl", "tk", "tcl", "libxml2", "expat" ],
             "download_url": "https://github.com/omnetpp/omnetpp/releases/download/omnetpp-3.3-ubuntu18.04/omnetpp-3.3-src-gcc73.tgz",
-            "patch_command": "sed -i '1s|.*|#!/bin/env perl|;2s|.*||' src/nedc/opp_msgc",
-            "setenv_command": "export PATH=$(pwd)/bin:$PATH && export LD_LIBRARY_PATH=$(pwd)/lib:$LD_LIBRARY_PATH && export TCL_LIBRARY=$(echo 'puts [info library]; exit' | wish)",
+            "setenv_command": "source setenv",
             "configure_command": "./configure",
             "build_command": "make MODE=release",
             "clean_command": "make clean"

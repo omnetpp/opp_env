8ce36a2d0a83f20e75da12c00d8044d9abde1e57
Andras Varga <andras@omnetpp.org>
2023-05-24 14:36:04 +0200

    omnetpp: added missing 5.4 and 5.5

--- opp_env/omnetpp_versions.py
+++ opp_env/omnetpp_versions.py
@@ -255,7 +255,7 @@ def make_omnetpp_project_description(version, base_version=None):
 def get_all_omnetpp_released_versions():
     released_versions = [
         "6.0.1", "6.0",
-        "5.7", "5.6.2", "5.6.1", "5.6", "5.5.1", "5.4.1", "5.3", "5.2.1", "5.2", "5.1.1", "5.1", "5.0", # note: no linux tarball on github for: 5.1, 5.2, etc
+        "5.7", "5.6.2", "5.6.1", "5.6", "5.5.1", "5.5", "5.4.1", "5.4", "5.3", "5.2.1", "5.2", "5.1.1", "5.1", "5.0", # note: no linux tarball on github for: 5.1, 5.2, etc
         "4.6", "4.5", "4.4.1", "4.4", "4.3.1", "4.3", "4.2.2", "4.2.1", "4.2", "4.1", "4.0p1", "4.0", # note: no linux tarball on github for: 4.0, 4.2, 4.2.1, 4.4
         "3.3p1"
     ]

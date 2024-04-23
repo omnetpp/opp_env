52451731327441071b3776014e253a991010930b
Rudolf Hornig <rudi@omnetpp.org>
2024-04-23 20:18:27 +0200

    flake: updated nix files
    - by default the opp_env package is pulled from the pypi repo.
    - nix shell .#opp_env-git builds from the git repository (i.e. a development version)

--- default.nix
+++ default.nix
@@ -1,27 +1,37 @@
-{ pkgs ? import <nixpkgs> {} }:
-let
-  version = "0.29.0.240422";
-in
-pkgs.python3Packages.buildPythonPackage {
-  name = "opp_env";
-  src = ./.;
+{ pname
+, version
+, src
+, python3Packages
+, lib 
+, nix
+, curl
+, git
+, btar }:
 
+python3Packages.buildPythonPackage {
+  inherit version src pname;
+  
+  pyproject = true;
+  disabled = python3Packages.pythonOlder "3.9";
+  
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
 
-  propagatedBuildInputs = with pkgs.python3Packages; [
-    pkgs.nix
-    pkgs.curl
-    pkgs.git
-    pkgs.btar
-  ];
+  propagatedBuildInputs = [ nix curl git btar ];
 
-  pyproject = true;
+  nativeBuildInputs = [ python3Packages.setuptools-scm ];
 
-  build-system = with pkgs.python3Packages; [ setuptools-scm ];
-
+  doCheck = true;
   pythonImportsCheck = [
     "opp_env.database"
   ];
 
-  doCheck = true;
+  meta = with lib; {
+    homepage= "https://github.com/omnetpp/opp_env";
+    description = "Tool for Automated Installation of OMNeT++ Simulation Frameworks";
+    longDescription = "opp_env is a powerful tool that allows for the easy and automated installation of OMNeT++ simulation frameworks and models, including dependencies like INET Framework and OMNeT++ itself. It can install any version of OMNeT++ and INET, as well as currently selected versions of Veins, SimuLTE, Simu5G and other models.";
+    changelog = "https://github.com/omnetpp/opp_env/blob/${version}/CHANGES.md";
+    license = licenses.lgpl3;
+    maintainers = [ "rudi@omnetpp.org" ];
+    platforms = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+  };
 }
--- flake.nix
+++ flake.nix
@@ -1,20 +1,38 @@
 {
-  description = "opp_env";
+  description = "opp_env: A Tool for Automated Installation of OMNeT++ Simulation Frameworks";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
+  outputs = { self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem(system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      pname = "opp_env";
+      version = "0.29.0.240422"; # Latest released version. Must be updated regularly.
+      githash = self.shortRev or "dirty";
+      timestamp = nixpkgs.lib.substring 0 8 self.lastModifiedDate;
+      gversion = "${githash}.${timestamp}";
+    in {
+      packages = rec {
+        opp_env = pkgs.callPackage ./default.nix {
+          inherit pname version;
+          src = pkgs.python3Packages.fetchPypi {
+            inherit pname version;
+            hash = "sha256-P1WNcB6yj78N0dQqN1u9o6aYQNyGr1aqwfTNqKbcxDo=";
+          };
+        };
 
-    packages.${system} = rec {
-      opp_env = pkgs.callPackage ./default.nix {};
-      default = opp_env;
-    };
+        opp_env-git = pkgs.callPackage ./default.nix {
+          pname = "${pname}-git";
+          version = "0+${gversion}";
+          src = ./.;
+        };
 
-  };
+        default = opp_env;
+      };
+
+    });
 }

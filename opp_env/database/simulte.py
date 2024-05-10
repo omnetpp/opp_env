fb95c074b47df7419b4a839cfdeae7cbc35bacc0
Gyorgy Szaszko <adam.szaszko@gmail.com>
2024-05-10 10:33:19 +0200

    simulte.py: added from-git option

--- opp_env/database/simulte.py
+++ opp_env/database/simulte.py
@@ -1,5 +1,7 @@
 
 def get_simulte_project_description(simulte_version, inet_versions, omnetpp_versions):
+    is_git_branch = simulte_version == "master"
+    git_branch_or_tag_name = f"v{simulte_version}" if simulte_version[0].isdigit() else simulte_version
     return {
         "name": "simulte", "version": simulte_version,
         "description": "LTE and LTE Advanced (3GPP Release 8 and beyond) user plane simulation model",
@@ -7,9 +9,6 @@ def get_simulte_project_description(simulte_version, inet_versions, omnetpp_vers
             "catalog_url": "https://omnetpp.org/download-items/SimuLTE.html",
         },
         "required_projects": {"inet": inet_versions, "omnetpp": omnetpp_versions},
-        "download_url":
-            f"https://github.com/inet-framework/simulte/releases/download/v{simulte_version}/simulte-{simulte_version}-src.tgz" if simulte_version == "1.2.0" else
-            f"https://github.com/inet-framework/simulte/archive/refs/tags/v{simulte_version}.tar.gz",
         "smoke_test_commands": [
             "cd simulations/demo",
             """if [ "$BUILD_MODE" = "debug" ]; then SIMULTE_LIB=$(echo $SIMULTE_ROOT/out/*-debug/src/*lte*); fi""",
@@ -28,7 +27,24 @@ def get_simulte_project_description(simulte_version, inet_versions, omnetpp_vers
             # Note: no setenv script in SimuLTE
         ],
         "build_commands": [ "make makefiles && make -j$NIX_BUILD_CORES MODE=$BUILD_MODE" ],
-        "clean_commands": [ "make clean" ]
+        "clean_commands": [ "make clean" ],
+        "options": {
+            "from-release": {
+                "option_description": "Install from release tarball on GitHub",
+                "option_category": "download",
+                "option_is_default": not is_git_branch,
+                "download_url": 
+                    f"https://github.com/inet-framework/simulte/releases/download/v{simulte_version}/simulte-{simulte_version}-src.tgz" if simulte_version == "1.2.0" else
+                    f"https://github.com/inet-framework/simulte/archive/refs/tags/v{simulte_version}.tar.gz",
+            },
+            "from-git": {
+                "option_description": "Install from git repo on GitHub",
+                "option_category": "download",
+                "option_is_default": is_git_branch,
+                "git_url": "https://github.com/inet-framework/simulte.git",
+                "git_branch": git_branch_or_tag_name,
+            },
+        },
     }
 
 def get_project_descriptions():
@@ -36,4 +52,5 @@ def get_project_descriptions():
         ["1.2.0", ["4.2.10", "4.2.5"], ["5.7.*"]], # Contains the integration code required to work with Veins.
         ["1.1.0", ["3.6.6"], ["5.5.*"]], # Contains the integration code required to work with Veins. Tested on Ubuntu 18.04. It is NOT compatible with INET 4.x.
         ["0.9.1", ["3.4.0"], ["5.1.*"]], # It DOES NOT contain support for the Veins simulator. Note: Release notes lists omnetpp-5.0 as dependency, but inet-3.4.0 requires omnetpp-5.1.
+        # ["master", ["4.3.2"], ["6.0.*"]],   # current master needs omnetpp-6.0pre10 and inet-4.3.2 which are not available in opp_env; doesn't build with inet-4.3.7 and omnetpp-6.0.*
     ]]

cf3c4110f01c87fb5fa067019391d11fc0ebead6
Andras Varga <andras@omnetpp.org>
2024-10-21 18:39:58 +0200

    testproject: added way to inject code into various commands

--- opp_env/database/testproject.py
+++ opp_env/database/testproject.py
@@ -3,12 +3,12 @@ def get_project_descriptions():
         {
             "name": "opp_env_testproject", "version": "0.1",
             "description": "Dummy project for testing opp_env",
-            "patch_commands": ["echo hello from patch command"],
-            "setenv_commands": ["echo hello from setenv command", "export SETENV_VAR=hello-from-setenv", "echo BUILD_MODES=$BUILD_MODES"],
-            "build_commands": ["make MODE=$BUILD_MODE"],
-            "clean_commands": ["make clean MODE=$BUILD_MODE"],
-            "test_commands":  ["make test"],
-            "smoke_test_commands": ["make smoketest"],
+            "patch_commands": ["$TEST_PRE_PATCH_COMMAND", "echo hello from patch command"],
+            "setenv_commands": ["$TEST_PRE_SETENV_COMMAND", "echo hello from setenv command", "export SETENV_VAR=hello-from-setenv", "echo BUILD_MODES=$BUILD_MODES"],
+            "build_commands": ["$TEST_PRE_BUILD_COMMAND", "make MODE=$BUILD_MODE"],
+            "clean_commands": ["$TEST_PRE_CLEAN_COMMAND", "make clean MODE=$BUILD_MODE"],
+            "test_commands":  ["$TEST_PRE_TEST_COMMAND", "make test"],
+            "smoke_test_commands": ["$TEST_PRE_SMOKETEST_COMMAND", "make smoketest"],
             "options": {
                 "from-release": {
                     "option_description": "Install from release tarball on GitHub",

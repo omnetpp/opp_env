a84192f69e5f513fa4e9fb1d6e0d1797d7c6ac4b
Gyorgy Szaszko <adam.szaszko@gmail.com>
2024-10-23 13:23:48 +0200

    inet.py: use $OPP_RUN_BIN* envvars for smoke test

--- opp_env/database/inet.py
+++ opp_env/database/inet.py
@@ -111,10 +111,10 @@ def make_inet_project_description(inet_version, omnetpp_versions):
         ],
         "smoke_test_commands": [
             "cd examples/ethernet/arptest",
-            """if [ "$mode" = "debug" ]; then DBG_SUFFIX="_dbg"; INET_LIB=$(echo $INET_ROOT/out/*-debug/src/*INET*); fi""",
-            """if [ "$mode" = "release" ]; then DBG_SUFFIX=""; INET_LIB=$(echo $INET_ROOT/out/*-release/src/*INET*); fi""",
-            "opp_run$DBG_SUFFIX -l $INET_LIB -n $INET_ROOT/tutorials:$INET_ROOT/examples:.:$INET_ROOT/src -c ARPTest -u Cmdenv --sim-time-limit=10s" if inet_version < "4.0" else
-            "inet$DBG_SUFFIX -c ARPTest -u Cmdenv --sim-time-limit=10s"
+            """if [ "$mode" = "debug" ]; then OPP_BIN=$OPP_RUN_DBG_BIN; INET_DBG_SUFFIX="_dbg"; INET_LIB=$(echo $INET_ROOT/out/*-debug/src/*INET*); fi""",
+            """if [ "$mode" = "release" ]; then OPP_BIN=$OPP_RUN_RELEASE_BIN; INET_DBG_SUFFIX=""; INET_LIB=$(echo $INET_ROOT/out/*-release/src/*INET*); fi""",
+            "$OPP_BIN -l $INET_LIB -n $INET_ROOT/tutorials:$INET_ROOT/examples:.:$INET_ROOT/src -c ARPTest -u Cmdenv --sim-time-limit=10s" if inet_version < "4.0" else
+            "inet$INET_DBG_SUFFIX -c ARPTest -u Cmdenv --sim-time-limit=10s"
         ],
         "test_commands": [
             "cd tests/fingerprint && ./fingerprinttest",

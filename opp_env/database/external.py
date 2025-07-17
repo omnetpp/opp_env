35b4b3c30ca79a86ace3cc5ff2724b0e24277e36
Gyorgy Szaszko <adam.szaszko@gmail.com>
2025-07-17 16:46:20 +0200

    external: added openflow-20250717

--- opp_env/database/external.py
+++ opp_env/database/external.py
@@ -1415,6 +1415,37 @@ def get_project_descriptions():
         },
 
         {
+            "name": "openflow", "version": "20250717",
+            "description": "OpenFlow Extension for INET Framework",
+            "details": "Ported to INET-4.x",
+            "metadata": {
+                "catalog_url": "https://omnetpp.org/download-items/Openflow.html",
+            },
+            "smoke_test_commands": [
+                r"""if [ "$BUILD_MODE" = "debug" ]; then BUILD_MODE_SUFFIX="_dbg"; fi""",
+                r"""cd scenarios/usa && opp_run$BUILD_MODE_SUFFIX -l $OPENFLOW_ROOT/src/OpenFlow -n $INET_ROOT/src:$OPENFLOW_ROOT/scenarios:.:../../src Scenario_USA_ARP_Ping_Drop.ini -u Cmdenv -r 0 --sim-time-limit=100s""",
+            ],
+            "required_projects": {"omnetpp": ["6.2.*", "6.1.*", "6.0.*"], "inet": ["4.5.4", "4.4.2"]},
+            "download_url": "https://github.com/inet-framework/openflow/archive/refs/tags/v20250717.tar.gz",
+            "patch_commands": [
+                r"""sed -i -E 's|-KINET_PROJ=[^ ]+|-KINET_PROJ=$(INET_ROOT) -o OpenFlow|' Makefile""",
+                r"""sed -i -E 's|--no-deep-includes||' Makefile""",
+                r"""sed -i 's|$DIR/../../inet|$INET_ROOT|' src/run_openflow""",
+                r"""sed -i 's|opp_run_dbg|opp_run|' src/run_openflow""",
+                r"""sed -i 's|scenarios:$DIR|scenarios:$DIR -i $OPENFLOW_ROOT/images|' src/run_openflow""",
+                r"""sed -i 's|DIR/openflow -n|DIR/OpenFlow -n|' src/run_openflow""",    # this is changed so that it matches SDN4CORE
+            ],
+            "setenv_commands": [
+                r"""export INET_PROJ=$INET_ROOT""",
+                r"""export PATH=$PATH:$OPENFLOW_ROOT/src""",
+                r"""export OMNETPP_IMAGE_PATH=$OMNETPP_IMAGE_PATH:$OPENFLOW_ROOT/images""",
+                r"""echo 'Hint: use the `run_openflow` command to run the examples in the scenarios folder.'"""
+            ],
+            "build_commands": [r"""make makefiles && make -j$NIX_BUILD_CORES MODE=$BUILD_MODE"""],
+            "clean_commands": [r"""make clean"""]
+        },
+
+        {
             "name": "openflow", "version": "20250605",
             "description": "OpenFlow Extension for INET Framework",
             "details": "Ported to INET-4.x",
